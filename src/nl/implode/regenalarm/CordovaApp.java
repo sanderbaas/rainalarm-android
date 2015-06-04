049b1eaa9f61c1aacfdef52e61eb1ff70c541b9b
Sander Baas <sander@implode.nl>
2015-06-04 16:01:13 +0200

    changed launchUrl

--- src/nl/implode/regenalarm/CordovaApp.java
+++ src/nl/implode/regenalarm/CordovaApp.java
@@ -29,7 +29,6 @@ public class CordovaApp extends CordovaActivity
     {
         super.onCreate(savedInstanceState);
         super.init();
-        // Set by <content src="index.html" /> in config.xml
-        loadUrl(launchUrl);
+        super.loadUrl("file:///android_asset/www/index.html", 2000);
     }
 }

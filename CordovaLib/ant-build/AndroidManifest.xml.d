7385b82ebfc838f9c6d9f687aaf5ce029da2af66
Sander Baas <sander@implode.nl>
2015-06-09 22:41:41 +0200

    removed binary libraries

--- CordovaLib/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/var/www/regenalarm/phonegap/rainalarm/platforms/android/CordovaLib/ant-build/AndroidManifest.xml : \
-/var/www/regenalarm/phonegap/rainalarm/platforms/android/CordovaLib/AndroidManifest.xml \
